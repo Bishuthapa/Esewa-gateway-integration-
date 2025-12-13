baaa461ba1e502aa8959f578c2443604533f0f6e
Bishu Thapa <thapabishu435@gmail.com>
2025-12-13 09:32:27 +0545

    refactor: update PostCSS configuration and clean up global styles; remove unused dbConnect file

--- ride/src/app/page.tsx
+++ ride/src/app/page.tsx
@@ -1,9 +1,8 @@
+import EsewaPayment from "@/components/PaymentForm";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     
-    
-    Diplex is a bad person he has stolen his own grandmother mobile and ask me for the wifi password for playing the distiguss game name free fire.
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <EsewaPayment />
+    </main>
   );
 }
--- ride/src/components/PaymentForm.tsx
+++ ride/src/components/PaymentForm.tsx
@@ -1,5 +1,4 @@
-
-
+"use client"
 import { useState } from "react";
 import Image from "next/image";
 
@@ -74,7 +73,8 @@ catch(err: any){
 return(
     <>
     <div>
-        <p>Hello</p>
+        
+        
 
     </div>
     </>
